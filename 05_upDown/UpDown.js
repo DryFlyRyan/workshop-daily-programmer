e8d88fe6fad410592e3d1c290d38a417685bd44a
Anna Mefford <annamefford@spencer-mefford-macbook-pro.local>
2016-08-04 19:48:22 -0600

    updown project

---
+++ 05_upDown/UpDown.js
@@ -0,0 +1,48 @@
+// # Up Down
+//
+// Given an array of numbers, write a program that describes whether each number was higher, lower or even to the previous number.
+//
+// ## Input
+//
+// An array of numbers (e.g [6,3,5,4,3,4,4,5])
+//
+// ## Output
+//
+// An array of up down strings (e.g. ["down","up","down","down","up","even","up"])
+
+//1. Turn numbers into an array
+//2. Turn values into numbers
+//3. Loop through the numbers.
+//4. if +1 = up, if -1 = down, if == even
+
+console.log('Up Down');
+
+var numbers = window.prompt('Enter in some Numbers!', 'enter them here...');
+
+function upDown(input){
+  var output = '';
+  var upDownArray = [];
+
+  var splitNumbers = numbers.split('');
+  console.log(splitNumbers);
+
+  var numberArray = splitNumbers.map(Number);
+  console.log(numberArray);
+
+  for (i=0; i<numberArray.length; i++){
+    if (numberArray[i] === numberArray[i-1]){
+      output = "even";
+      upDownArray.push(output);
+    } else if (numberArray[i] < numberArray[i-1]){
+      output = "down";
+      upDownArray.push(output);
+    } else if (numberArray[i] > numberArray[i-1]){
+      output = "up"
+      upDownArray.push(output);
+    }
+  }
+console.log(upDownArray);
+return upDownArray;
+}
+
+console.log(upDown(numbers));
