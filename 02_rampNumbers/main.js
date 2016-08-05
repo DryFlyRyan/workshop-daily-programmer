be7b7e929fc2dbaac47784ee25561050b7244c9a
Brandon Oney <brandon.2.oney@ucdenver.edu>
2016-08-04 18:31:24 -0600

    Removed Extra Spaces

--- 02_rampNumbers/main.js
+++ 02_rampNumbers/main.js
@@ -3,10 +3,8 @@ function rampNumberCheck(number) {
     for (var i = 0; i < number.length - 1; i++) {
         if ((Number(number[i]) + 1 === Number(number[i + 1]) || (Number(number[i]) - 1) === Number(number[i + 1])) && isRampNumber === true) {
             isRampNumber = true;
-            console.log("true");
         } else {
             isRampNumber = false;
-            console.log("false");
         }
     }
     return isRampNumber
