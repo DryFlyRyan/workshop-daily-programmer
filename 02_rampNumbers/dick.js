a48a66dd40acd7d8234751365f16075e4e788245
dickricd <rdickerson2@gmail.com>
2016-08-04 19:53:58 -0600

    whew, got it (ramp number)

--- 02_rampNumbers/dick.js
+++ 02_rampNumbers/dick.js
@@ -1,1 +1,23 @@
-d
+//ramp numbers n stuff
+
+var digits = prompt("input some numbers");
+var array = [];
+
+   
+function ramper(array) 
+{
+    
+    for(var a=0; a<array.length-1; a++)
+        {
+            if( Number(array[a]) > Number(array[a+1]) )
+             {   
+                 return false;
+             }
+        }
+        return true;
+}
+
+console.log("The entered number is a ramp number? " + ramper(digits) );
+ 
+
+   
