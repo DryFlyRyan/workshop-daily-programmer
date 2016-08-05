338aef494f8325f78693b1b738f14ff4f4f33b6a
Amanda Herschleb <amanda.herschleb@gmail.com>
2016-08-04 19:49:16 -0600

    up down done

---
+++ 05_upDown/AMH.js
@@ -0,0 +1,25 @@
+
+// Daily Programmer 05 - Up Down
+////////////////////////////////////////////
+
+inputArray = [1,5,7,3,4,9,1,0];
+upDownArray = [];
+
+
+function upDown(array){
+  var upDown;
+
+  for(var i = 0; i<array.length -1; i++){
+    if(array[i] > array[i+1]){
+      upDown = "down";
+    }else if(array[i] < array[i+1]){
+      upDown = "up";
+    }else{
+      upDown = "same";
+    }
+    upDownArray.push(upDown);
+  }
+  return upDownArray;
+}
+
+console.log(upDown(inputArray));
